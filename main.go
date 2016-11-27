753cc6b0b9c640a46ec8dc8de3a57d59a8b435e1
Toomore Chiang <toomore0929@gmail.com>
2016-11-27 14:24:19 +0000

    Add photo page (dev)

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"math"
 	"net/http"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/toomore/lazyflickrgo/flickr"
@@ -14,14 +16,26 @@ import (
 )
 
 var (
-	f      *flickr.Flickr
-	rTags  [10]string
-	tpl    *template.Template
-	userID string
+	f             *flickr.Flickr
+	rTags         [10]string
+	tplIndex      *template.Template
+	tplPhoto      *template.Template
+	userID        string
+	photoPageExpr = regexp.MustCompile(`/p/([0-9]+)-?(.+)?`)
 )
 
 func init() {
-	tpl, _ = template.ParseFiles("./base.htm")
+	funcs := template.FuncMap{
+		"isHTML": func(content string) (template.HTML, error) {
+			return template.HTML(content), nil
+		},
+	}
+	tplIndex, _ = template.ParseFiles("./base.htm")
+	tplIndex, _ = tplIndex.ParseFiles("./index.htm")
+	tplPhoto, _ = template.ParseFiles("./base.htm")
+	tplPhoto = tplPhoto.Funcs(funcs)
+	tplPhoto, _ = tplPhoto.ParseFiles("./photo.htm")
+
 	f = flickr.NewFlickr(os.Getenv("FLICKRAPIKEY"), os.Getenv("FLICKRSECRET"))
 	f.AuthToken = os.Getenv("FLICKRUSERTOKEN")
 	userID = os.Getenv("FLICKRUSER")
@@ -80,15 +94,39 @@ func index(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.Header().Set("ETag", etagStr)
 		w.Header().Set("Cache-Control", "max-age=60")
-		tpl.Execute(w, fromSearch(rTags[modValue]))
+		tplIndex.Execute(w, fromSearch(rTags[modValue]))
 	}
+}
+
+func photo(w http.ResponseWriter, r *http.Request) {
+	logs(r)
+	log.Println(r.RequestURI)
+	match := photoPageExpr.FindStringSubmatch(r.RequestURI)
+	var photono string
+	if len(match) >= 2 {
+		photono = match[1]
+	}
+
+	var photoinfo jsonstruct.PhotosGetInfo
+	if photono != "" {
+		photoinfo = f.PhotosGetInfo(photono)
+		if photoinfo.Common.Stat == "ok" {
+			photoinfo.Photo.Description.Content = strings.Replace(photoinfo.Photo.Description.Content, "\n", "<br>", -1)
+			tplPhoto.Execute(w, photoinfo.Photo)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+	log.Printf("%+v", photoinfo)
 }
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/p/", photo)
 	//http.Handle("/static", http.FileServer(http.Dir("./static/")))
 	serveSingle("/favicon.ico", "favicon.ico")
 	serveSingle("/jquery.unveil.min.js", "jquery.unveil.min.js")
 	serveSingle("/base_min.css", "base_min.css")
-	log.Println(http.ListenAndServe(":8080", nil))
+	serveSingle("/base.css", "base.css")
+	log.Println(http.ListenAndServe(":8888", nil))
 }
